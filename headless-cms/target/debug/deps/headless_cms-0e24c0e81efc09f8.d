4bee9ac97a3275d4cfb669c6b33a97b1984092a7
bisaek <huggerbertram@gmail.com>
2023-02-07 19:29:27 +0100

    cargo

---
+++ headless-cms/target/debug/deps/headless_cms-0e24c0e81efc09f8.d
@@ -0,0 +1,5 @@
+/home/bisaek/Skrivebord/headless-cms-rust/headless-cms/target/debug/deps/headless_cms-0e24c0e81efc09f8: src/main.rs
+
+/home/bisaek/Skrivebord/headless-cms-rust/headless-cms/target/debug/deps/headless_cms-0e24c0e81efc09f8.d: src/main.rs
+
+src/main.rs:
---
+++ headless-cms/target/debug/headless-cms.d
@@ -0,0 +1,1 @@
+/home/bisaek/Skrivebord/headless-cms-rust/headless-cms/target/debug/headless-cms: /home/bisaek/Skrivebord/headless-cms-rust/headless-cms/src/main.rs
