88d9862b6e85a3951783e1ff79374eed85d5d177
bisaek <huggerbertram@gmail.com>
2023-02-12 22:38:16 +0100

    add to db

--- headless-cms/src/api/create_page_type.rs
+++ headless-cms/src/api/create_page_type.rs
@@ -1,4 +1,7 @@
+use crate::db::establish_connection;
+use crate::models::page_type::{self, NewPageType};
 use actix_web::{post, web::Json, HttpResponse, Responder};
+use diesel::prelude::*;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -10,6 +13,7 @@ pub struct Field {
 #[derive(Deserialize)]
 pub struct PageType {
     fields: Vec<Field>,
+    name: String,
 }
 // impl PageType {
 //     fn newField(&mut self, fieldType: String, name: String) {
@@ -20,8 +24,25 @@ pub struct PageType {
 //     }
 // }
 
+pub fn add_to_db(info: Json<PageType>) {
+    println!("{}", info.name);
+
+    // use crate::schema::page_type;
+    use crate::schema::page_type::dsl::*;
+
+    let mut connection = establish_connection();
+
+    let new_page_type = NewPageType { name: &info.name };
+
+    diesel::insert_into(crate::schema::page_type::dsl::page_type)
+        .values(&new_page_type)
+        .execute(&mut connection)
+        .expect("Error saving new page type");
+}
+
 #[post("createPageType")]
-pub async fn create_page_type(page_type: Json<PageType>) -> impl Responder {
-    println!("{}", page_type.fields[3].name);
+pub async fn post(info: Json<PageType>) -> impl Responder {
+    // println!("{}", page_type.fields[3].name);
+    add_to_db(info);
     HttpResponse::Ok().body("new page type")
 }
